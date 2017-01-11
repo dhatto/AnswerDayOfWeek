2da5f006234d896661730006a98547664d3ae242
dhatto <ramenplease@gmail.com>
2017-01-11 21:54:44 +0900

    first commit

---
+++ AnswerDayOfWeek/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AnswerDayOfWeek
+//
+//  Created by hattori on 2017/01/11.
+//  Copyright © 2017年 com.example. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
